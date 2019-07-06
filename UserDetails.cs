ec84e21fd9b96c172ff98dfcd7cd5da049e29c8d
GhaidaSenani <46917360+GhaidaSenani@users.noreply.github.com>
2019-07-07 01:27:23 +0300

    Add files via upload

---
+++ UserDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBotTest
+{
+    public class UserDetails
+    {
+
+        public String CaseNo { get; set; }
+
+        public String Court { get; set; }
+
+        public String Year { get; set; }
+        public String ReqCode { get; set; }
+        public String NID { get; set; }
+        public String DOB { get; set; }
+        public String PhoneNo { get; set; }
+    }
+}
+
